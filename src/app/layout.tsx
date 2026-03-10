a7f1583a2bda55b79bddd468aa7d428545c6ed0b
johnkennedyb <johnkennedy3313@gmail..com>
2026-03-10 11:22:59 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
             { label: "Industry", href: "/category/industry" },
           ]}
         />
-        <section className="relative mt-[14px] sm:mt-5">
+        <section className="relative mt-[14px] sm:mt-[23px]">
           <div className="mx-auto w-full max-w-6xl px-4 py-6 md:py-8">{children}</div>
         </section>
         <SiteFooter />
