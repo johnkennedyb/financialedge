ba59632e0dfc2866349ce5e452d1ca26b73111d0
johnkennedyb <johnkennedy3313@gmail..com>
2026-03-10 11:09:41 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
             { label: "Industry", href: "/category/industry" },
           ]}
         />
-        <section className="relative mt-5">
+        <section className="relative" style={{ marginTop: '10px' }}>
           <div className="mx-auto w-full max-w-6xl px-4 py-6 md:py-8">{children}</div>
         </section>
         <SiteFooter />
